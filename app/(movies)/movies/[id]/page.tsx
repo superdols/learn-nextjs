ce5e80ff85974f6da73455adf8dabf42a14864c2
superdols <superdols37@gmail.com>
2024-09-23 17:12:55 +0900

    movie detail styling

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,19 +1,26 @@
 import { Suspense } from "react"
-import MovieInfo from "../../../../components/movie-info"
+import MovieInfo, { getMovie } from "../../../../components/movie-info"
 import MovieVideos from "../../../../components/movie-videos"
+
+interface IParams {
+  params: {id: string};
+}
+
+export async function generateMetadata({params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+   }
+}
 
 export default async function MovieDetail({
     params: { id }
-  }: {
-    params: {id: string}
-  }) {
+  }: IParams) {
     return (
       <div>
-        <h3>Mmovie Detail Page</h3>
         <Suspense fallback={<h1>Loading movie info</h1>}>
           <MovieInfo id={id} />
         </Suspense>
-        <h4>Videos</h4>
         <Suspense fallback={<h1>Loading movie videos</h1>}>
           <MovieVideos id={id} />
         </Suspense>
