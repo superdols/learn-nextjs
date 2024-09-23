3d82b8779ff8ee0991b7f4ba5ef44fac1fb24557
superdols <superdols37@gmail.com>
2024-09-23 15:36:45 +0900

    first develop

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Error() {
+  return <h1>lol something borke...</h1>
+};
---
+++ app/(movies)/movies/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loding() {
+  return <h2>Loading a movie :id</h2>
+};
